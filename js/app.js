e0eb351aca374c3f68e26e7af2285d96e39e9e93
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 10:54:00 +0100

    Add comments to explain what program does

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 // Tu código aquí
+
+//Contador a incrementar y disminuir
 let counter = 0;
+
+//Boton de incrementar assignado
 let plusNum = document.querySelector(".nextBtn");
+//Boton de disminuir assignado
 let minusNum = document.querySelector(".prevBtn");
 
+//Event listener al hacer click, nos aumenta el contador y luyego lo printa
+//en el selector con id counter
 plusNum.addEventListener("click", function () {
   counter++;
   document.querySelector("#counter").innerHTML = counter.toString();
 });
 
+//Event listener al hacer click, nos disminuye el contador y luyego lo printa
+//en el selector con id counter
 minusNum.addEventListener("click", function () {
   counter--;
   document.querySelector("#counter").innerHTML = counter.toString();
