fa64f54efbb7136b5249cb1bc361d095ee14a863
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 10:47:59 +0100

    Exercice finish

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 // Tu código aquí
+let counter = 0;
+let plusNum = document.querySelector(".nextBtn");
+let minusNum = document.querySelector(".prevBtn");
+
+plusNum.addEventListener("click", function () {
+  counter++;
+  document.querySelector("#counter").innerHTML = counter.toString();
+});
+
+minusNum.addEventListener("click", function () {
+  counter--;
+  document.querySelector("#counter").innerHTML = counter.toString();
+});
